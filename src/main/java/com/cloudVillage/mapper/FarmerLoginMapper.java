0a0783b4de71d35d19d1c1a0fd6406315fd3dd56
Link <61148194+Linkbreathe@users.noreply.github.com>
2022-05-29 17:09:16 +0800

    2022/5/29 Link 农户登录
    Signed-off-by: Link <61148194+Linkbreathe@users.noreply.github.com>

---
+++ src/main/java/com/cloudVillage/mapper/FarmerLoginMapper.java
@@ -0,0 +1,15 @@
+package com.cloudVillage.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import com.cloudVillage.entity.FarmerLogin;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Link
+ * @Date: 2022/05/29/14:50
+ * @Description:
+ */
+public interface FarmerLoginMapper extends BaseMapper<FarmerLogin> {
+}
